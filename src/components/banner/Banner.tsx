d544aea23c66ba9c6e58191ca85f91046d00c257
Deborah Lyra <deborahp.lyra@gmail.com>
2024-10-12 13:06:55 -0300

    adjust fa-in

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -3,21 +3,30 @@ import photo1 from '/public/static/banner-photo-1.png';
 import photo2 from '/public/static/banner-photo-2.png';
 import photo3 from '/public/static/banner-photo-3.png';
 
-const images = [photo1, photo2, photo3]; 
+const images = [photo1, photo2, photo3];
 
 export const Banner = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [fade, setFade] = useState(true); 
 
   const goToPrevious = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? images.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
+    setFade(false); 
+    setTimeout(() => {
+      const isFirstSlide = currentIndex === 0;
+      const newIndex = isFirstSlide ? images.length - 1 : currentIndex - 1;
+      setCurrentIndex(newIndex);
+      setFade(true); 
+    }, 300); 
   };
 
   const goToNext = () => {
-    const isLastSlide = currentIndex === images.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
+    setFade(false); 
+    setTimeout(() => {
+      const isLastSlide = currentIndex === images.length - 1;
+      const newIndex = isLastSlide ? 0 : currentIndex + 1;
+      setCurrentIndex(newIndex);
+      setFade(true); 
+    }, 300); 
   };
 
   const getBackgroundPosition = () => {
@@ -33,10 +42,10 @@ export const Banner = () => {
   }, [currentIndex]);
 
   return (
-    <div className="max-w-full h-[200px] relative group md:h-[500px] lg:h-[550px] mt-0 md:mt-4 lg:mt-6">
+    <div className="max-w-full h-[450px] relative group md:h-[500px] lg:h-[550px] mt-0 md:mt-4 lg:mt-6">
       {/* Imagem */}
       <div
-        className={`w-full h-full bg-contain bg-center transition-all duration-500 ease-in-out bg-no-repeat md:bg-cover lg:bg-cover ${getBackgroundPosition()}`}
+        className={`w-full h-full bg-contain bg-center transition-opacity duration-500 ease-in-out bg-no-repeat md:bg-cover lg:bg-cover ${fade ? "opacity-100" : "opacity-0"} ${getBackgroundPosition()}`}
         style={{ backgroundImage: `url(${images[currentIndex]})` }}
       ></div>
 
@@ -62,7 +71,7 @@ export const Banner = () => {
           <div
             key={index}
             onClick={() => setCurrentIndex(index)}
-            className={`w-2 h-2 md:w-3 md:h-3 lg:w-4 lg:h-4 rounded-full cursor-pointer ${
+            className={`w-3 h-3 md:w-4 md:h-4 lg:w-5 lg:h-5 rounded-full cursor-pointer ${
               currentIndex === index ? "bg-sky-700" : "bg-blue-800"
             }`}
           />
