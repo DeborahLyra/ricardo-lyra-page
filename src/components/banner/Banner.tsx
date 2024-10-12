dfe36f24e5e4a024c97f65566deab528dbdc1bc3
Deborah Lyra <deborahp.lyra@gmail.com>
2024-10-12 12:52:57 -0300

    canged banner

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,22 +1,73 @@
-import { BannerContainer, DivButtons } from "./style";
-import photoBanner from '/public/static/ricardo-lyra-profile.jpeg'
+import React, { useState, useEffect } from "react";
+import photo1 from '/public/static/banner-photo-1.png';
+import photo2 from '/public/static/banner-photo-2.png';
+import photo3 from '/public/static/banner-photo-3.png';
 
-export function Banner() {
+const images = [photo1, photo2, photo3];
+
+export const Banner = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? images.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToNext = () => {
+    const isLastSlide = currentIndex === images.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const getBackgroundPosition = () => {
+    return currentIndex === 0 ? "md:bg-top lg:bg-top" : "";
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      goToNext(); 
+    }, 4000);
+
+    return () => clearInterval(interval); 
+  }, [currentIndex]);
+
   return (
-    <BannerContainer >
-      <div>
-        <h1>Ricardo Lyra</h1>
-        <p>Médico Urologista e Andrologista</p>
-        <strong>
-           CRM 9981- PE / CRM 13513-PB <br />
-            RQE 5549 <br />
-           </strong>
-        <DivButtons>
-           <a href="#contact"> <button>Agende uma consulta</button></a>
-            <a href="#specialization"><button>Especialidades</button></a>
-        </DivButtons>
+    <div className="max-w-full h-[200px] relative group md:h-[500px] lg:h-[550px] mt-0 md:mt-4 lg:mt-6">
+      {/* Imagem */}
+      <div
+        className={`w-full h-full bg-contain bg-center transition-all duration-500 ease-in-out bg-no-repeat md:bg-cover lg:bg-cover ${getBackgroundPosition()}`}
+        style={{ backgroundImage: `url(${images[currentIndex]})` }}
+      ></div>
+
+      {/* Botão para voltar */}
+      <button
+        onClick={goToPrevious}
+        className="absolute top-1/2 left-2 -translate-y-1/2 text-white p-3 md:p-4 lg:p-6 rounded-full lg:hover:bg-blue-800 transition"
+      >
+        ❮
+      </button>
+
+      {/* Botão para avançar */}
+      <button
+        onClick={goToNext}
+        className="absolute top-1/2 right-2 -translate-y-1/2 text-white p-3 md:p-4 lg:p-6 rounded-full lg:hover:bg-blue-800 transition"
+      >
+        ❯
+      </button>
+
+      {/* Indicadores */}
+      <div className="absolute bottom-2 md:bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2 md:space-x-3">
+        {images.map((_, index) => (
+          <div
+            key={index}
+            onClick={() => setCurrentIndex(index)}
+            className={`w-2 h-2 md:w-3 md:h-3 lg:w-4 lg:h-4 rounded-full cursor-pointer ${
+              currentIndex === index ? "bg-sky-700" : "bg-blue-800"
+            }`}
+          />
+        ))}
       </div>
-      <img src={photoBanner} alt="Ricardo Lyra Profile" />
-    </BannerContainer>
-  )
-}
+    </div>
+  );
+};
